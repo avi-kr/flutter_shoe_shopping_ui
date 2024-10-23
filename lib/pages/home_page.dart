cdec343dbdd13db28f85204468a5f01d86e6bbbc
Abhishek Kumar <avi.kr16@gmail.com>
2024-10-23 16:06:03 +0530

    Home page bottom nav bar created.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app_ui/components/bottom_nav_bar.dart';
+import 'package:shopping_app_ui/pages/shop_page.dart';
+
+import 'cart_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +12,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // this selected index is to control the bottom nav bar
+  int _selectedIndex = 0;
+
+  // this method will update our selected index
+  // when the user taps on the bottom bar
+  void navigationBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [
+    const ShopPage(),
+    const CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: BottomNavBar(
+        onTabChange: (index) => navigationBottomBar(index),
+      ),
+      body: _pages[_selectedIndex],
+    );
   }
 }
